b5a4afe7b12ed6d191ccc4dd307d75e340382ecd
Francis Chartrand <chartrandf@users.noreply.github.com>
2026-05-21 15:53:49 -0400

    refactor(sdk): rename tenant switch APIs to reseller/location

--- src/app.ts
+++ src/app.ts
@@ -65,8 +65,8 @@ import {
   EVENT_ON_CONNECTED_TO_STACK,
   EVENT_ON_SWITCH_STACK_TENANT,
   EVENT_CHANGE_TOOLBAR_DISPLAY,
-  EVENT_SWITCH_PORTAL_TENANT,
-  EVENT_SWITCH_STACK_TENANT,
+  EVENT_SWITCH_PORTAL_RESELLER,
+  EVENT_SWITCH_STACK_LOCATION,
   EVENT_MOBILE_HEADER,
   EVENT_MOBILE_CONTEXTUAL_MENU,
   EVENT_MOBILE_SHOW_BOTTOM_NAV,
@@ -281,9 +281,9 @@ export class App {
   // Portal
   changeToolbarDisplay = (display: boolean) => this._sendMessage(EVENT_CHANGE_TOOLBAR_DISPLAY, { display });
 
-  switchPortalTenant = (uuid: string) => this._sendMessage(EVENT_SWITCH_PORTAL_TENANT, { tenant: { uuid } });
+  switchPortalReseller = (uuid: string) => this._sendMessage(EVENT_SWITCH_PORTAL_RESELLER, { reseller: { uuid } });
 
-  switchStackTenant = (uuid: string) => this._sendMessage(EVENT_SWITCH_STACK_TENANT, { tenant: { uuid } });
+  switchStackLocation = (uuid: string) => this._sendMessage(EVENT_SWITCH_STACK_LOCATION, { location: { uuid } });
 
   // App to Mobile
   setMobileHeader = ({ title, callback }: { title?: string | null, callback?: Function | null }, entityId?: string) => {
